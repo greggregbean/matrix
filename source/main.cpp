b65d3f169d0f5267683f8d124ecea3e174de2cfe
Zinin Ivan <90098955+greggregbean@users.noreply.github.com>
2023-04-11 01:13:55 +0300

    small changes

--- source/main.cpp
+++ source/main.cpp
@@ -8,36 +8,24 @@ int main() {
     // Square Matrix 
     //---------------
         int size;
-        std::cout << "Enter size:" << std::endl;
         std::cin >> size;
-        std::cout << std::endl;
 
         long double* numbers = new long double [size * size];
 
-        std::cout << "Enter data of matrix:" << std::endl;
-        for(int i = 0; i < size; ++i) {
-            for(int j = 0; j < size; ++j) {
-                std::cin >> numbers [size*i + j];
-            }
+        for(int i = 0; i < size * size; ++i) {
+            std::cin >> numbers [i];
         }
-        std::cout << std::endl;
 
-        std::cout << "Square matrix:" << std::endl;
         square_matrix<long double> square (size, numbers);
-        square.checkout();
         delete numbers;
     
-
-
     //-------------------
     // Minor determinant
     //-------------------
-        std::cout << "Minor determinant = " <<  square.minor_det() << "\n" << std::endl;
+        // std::cout <<  square.minor_det() << std::endl;
 
-    
     //------------------
     // Gauss detrminant
     //------------------
-        long double res_of_gauss_det = square.gauss_det();
-        std::cout << "Gauss determinant = " << res_of_gauss_det << "\n" << std::endl;
+        std::cout << square.gauss_det() << std::endl;
 }
